79995fc1dd77b958d7048e4e874097d40564494d
abdellah khouden <khoudenak@gmail.com>
2025-01-04 13:33:55 +0100

    Calculate and save total price for each Commande based on associated Produits

--- .tinkerun/tinker.php
+++ .tinkerun/tinker.php
@@ -1,4 +1,14 @@
 <?php
 
-namespace App\Models;
+use App\Models\Commande;
 
+$commandes = Commande::with('produits')->get();
+
+foreach ($commandes as $commande) {
+  $totalProduitPrix = 0;
+  foreach($commande->produits as $produit){
+    $totalProduitPrix += $produit->prix * $produit->pivot->quantite;
+  }
+  $commande->montant = $totalProduitPrix;
+  $commande->save();
+}
